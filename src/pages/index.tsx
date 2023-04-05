35d5e451462d2bbd147db62083e9a326bf3a54fd
Thomas <thekileyenator@gmail.com>
2023-04-05 20:28:38 +0100

    A really basic splash page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,34 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
+import "../css/main_style.css"
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+export const Head: HeadFC = () => (
+    <>
+        <title>Oxford Growers Todo</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin=""/>
+        <link href="https://fonts.googleapis.com/css2?family=Signika+Negative:wght@300&amp;display=swap" rel="stylesheet"/>
+    </>
+)
 
 const IndexPage: React.FC<PageProps> = () => {
-  return (
-    <main style={pageStyles}>
-    </main>
-  )
+    return (
+        <main>
+            <div id="centre">
+                <div id="header">
+                    <div id="header-text">
+                        <h1>Oxford Growers Network</h1>
+                    </div>
+                </div>
+                <div id="content">
+                    <input placeholder="Project Name"></input>
+                    <p><a href="https://oxfordgrowersnetwork.github.io" target="_blank">Learn more about the Oxford Growers Network</a></p>
+                </div>
+                <div id="footer">
+                </div>
+            </div>
+        </main>
+    )
 }
 
 export default IndexPage
-
-export const Head: HeadFC = () => <title>Home Page</title>
